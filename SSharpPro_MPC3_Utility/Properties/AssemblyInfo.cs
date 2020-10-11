dafece4ea4399018a38219449d1189038199a1a2
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2020-10-11 01:04:00 -0500

    delete cpz

--- SSharpPro_MPC3_Utility/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SSharpPro_MPC3_Utility")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSharpPro_MPC3_Utility")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
